f80eb585de974cd137c18f14aeac8ceb6d273ed2
alihamza126 <alihamzageo75@gmail.com>
2025-05-05 04:04:31 +0500

    strcturize folderrs

---
+++ src/app/page.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import React from 'react'
+import TopNavThree from '@/components/Header/TopNav/TopNavThree'
+import MenuTwo from '@/components/Header/Menu/MenuTwo'
+import SliderThree from '@/components/Slider/SliderThree'
+import BannerTop from '@/components/Home1/BannerTop'
+import TrendingProduct from '@/components/Home1/TrendingProduct'
+import productData from '@/data/Product.json'
+import Benefit from '@/components/Home1/Benefit'
+import FlashSale from '@/components/Home1/FlashSale'
+import Brand from '@/components/Home1/Brand'
+import Footer from '@/components/Footer/Footer'
+import ModalNewsletter from '@/components/Modal/ModalNewsletter'
+import TrendingNow from '@/components/Home1/TrendingNow'
+import testimonialData from '@/data/Testimonial.json'
+import Testimonial from '@/components/Home1/Testimonial'
+import Deal from '@/components/Home1/Deal'
+import SliderTwo from '@/components/Slider/SliderTwo'
+import SliderOne from '@/components/Slider/SliderOne'
+
+
+
+export default function Home() {
+    return (
+        <>
+            <TopNavThree props="style-three bg-white" />
+            <div id="header" className='relative w-full'>
+                <MenuTwo />
+                <BannerTop props="bg-black py-3" textColor='text-white' bgLine='bg-white' />
+                <SliderOne />
+            </div>
+            <TrendingNow />
+            <Deal data={productData} start={4} limit={8} />
+            <TrendingProduct data={productData} start={10} limit={18} />
+            <FlashSale />
+            
+            <Testimonial data={testimonialData} limit={5} />
+            <Benefit props="md:mt-20 mt-10 py-10 px-2.5 bg-surface rounded-3xl" />
+            <Brand />
+            <Footer />
+            <ModalNewsletter />
+        </>
+    )
+}
